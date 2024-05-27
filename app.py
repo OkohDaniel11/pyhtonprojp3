16984e04a6673fe47b73ba084b0e26b216f355c2
OkohDaniel11 <129377762+OkohDaniel11@users.noreply.github.com>
2024-05-27 14:44:24 +0100

    initial commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -283,6 +284,7 @@ def play_game():
     return do_you_want_to_play()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
 
 
